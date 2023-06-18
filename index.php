8ee3d2c1f54d72c0016504a55376a60d85583efa
Sandeep Sharma <sandeepsharma368@gmail.com>
2023-06-18 20:51:07 +0000

    3rd push

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
       document.getElementById("signupButton").style.display = "none";
       document.getElementById("signInButton").style.display = "none";
       document.getElementById("welcomeText").innerHTML = ""; // Remove the welcome message
+      document.getElementById("signinText").style.display = "block"; // Show the "Already have an account?" text
     }
   </script>
 </head>
@@ -26,46 +27,48 @@
       <li>Policy</li>
     </ul>
   </div>
-  
+
   <div id="homeScreen" class="container">
     <h1 id="welcomeText">Welcome to tAnGO ties</h1>
     <!-- Contents of the home screen -->
     <button id="signupButton" onclick="showSignUpForm()">Sign Up</button>
     <button id="signInButton" onclick="window.location.href = 'signin.php'">Sign In</button>
-  </div>
+    <div id="signupForm" class="signupBox" style="display: none;">
+      <h2>Sign Up</h2>
+      <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
 
-  <div id="signupForm" class="signupBox">
-    <h2>Sign Up</h2>
-    <form>
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="name" required>
+        <label for="phone">Phone Number:</label>
+        <input type="text" id="phone" name="phone" required>
 
-      <label for="phone">Phone Number:</label>
-      <input type="text" id="phone" name="phone" required>
+        <label for="email">Email Address:</label>
+        <input type="email" id="email" name="email" required>
 
-      <label for="email">Email Address:</label>
-      <input type="email" id="email" name="email" required>
+        <label for="gender">Gender:</label>
+        <select id="gender" name="gender" required>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="others">Others</option>
+        </select>
 
-      <label for="gender">Gender:</label>
-      <select id="gender" name="gender" required>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="others">Others</option>
-      </select>
+        <label for="age">Age:</label>
+        <select id="age" name="age" required>
+          <option value="">Select Age</option>
+          <!-- Generate options from 18 to 65 -->
+          <?php
+            for ($i = 18; $i <= 65; $i++) {
+              echo "<option value='$i'>$i</option>";
+            }
+          ?>
+        </select>
 
-      <label for="age">Age:</label>
-      <select id="age" name="age" required>
-        <option value="">Select Age</option>
-        <!-- Generate options from 18 to 65 -->
-        <?php
-          for ($i = 18; $i <= 65; $i++) {
-            echo "<option value='$i'>$i</option>";
-          }
-        ?>
-      </select>
-
-      <input type="submit" class="submitButton" value="Submit">
-    </form>
+        <input type="submit" class="submitButton" value="Submit">
+        <div id="signinText" class="signinText" style="display: none;">
+          Already have an account? <a href="signin.php">Sign In</a>
+        </div>
+      </form>
+    </div>
   </div>
 
   <script>
